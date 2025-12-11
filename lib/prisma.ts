1d9a752a2fd17403463e0289e23bf84088d34989
Mike M <mikester70387@gmail.com>
2025-12-11 02:00:22 -0800

    Add pg adapter

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,3 +1,4 @@
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "~/prisma/generated/client";
 
 export * from "~/prisma/generated/models";
@@ -6,6 +7,7 @@ const globalForPrisma = global as unknown as {
 	prisma?: PrismaClient;
 };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
