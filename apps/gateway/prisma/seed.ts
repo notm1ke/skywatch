c6055b858edf00fe8814f6ed3993e7e873a06712
Mike M <mikester70387@gmail.com>
2026-01-26 18:36:59 -0800

    Add planes to seed step

--- apps/gateway/prisma/seed.ts
+++ apps/gateway/prisma/seed.ts
@@ -1,5 +1,6 @@
 import { redis } from "@/services/redis";
 import { prisma } from "@/services/prisma";
+import { seedPlanes } from "./seed/planes";
 import { seedAirports } from "./seed/airport";
 import { seedAirlineHubs } from "./seed/airline-hubs";
 import { seedAirportHasRvrs } from "./seed/airport-rvr";
@@ -33,6 +34,9 @@ await seedAirportHasAtis();
 await seedAirportHasClear();
 await seedAirportHasPrecheck();
 await seedAirportHasRvrs();
+
+// planes
+await seedPlanes();
 
 console.log(`Finished seeding database in ${(Date.now() - start).toFixed(2)}ms.`);
 await redis.set("db:seed", start);
