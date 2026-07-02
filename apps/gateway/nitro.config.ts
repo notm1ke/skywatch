a51c15d023b6435778cec4ed97b40710277bc53a
Mike Medved <me@m1ke.co>
2026-07-02 00:31:58 -0400

    Revert

--- apps/gateway/nitro.config.ts
+++ apps/gateway/nitro.config.ts
@@ -1,13 +1,8 @@
 import { defineConfig } from "nitro";
-import { fileURLToPath, URL } from "url";
 
 export default defineConfig({
 	serverDir: ".",
 	routes: { "/**": "./src/index.ts" },
-	alias: {
-		"@/prisma": fileURLToPath(new URL("./prisma", import.meta.url)),
-		"@": fileURLToPath(new URL("./src", import.meta.url)),
-	},
 	experimental: {
 		tasks: true
 	},
